20a3ed718658402ad9e9726f20e1744d21aa1c8c
MinThitNaing <mr.minthitnaing@gmail.com>
2023-08-11 15:49:06 +0000

    Changed index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var my_name = prompt("What is your name?: ");
+var your_name = prompt("What is your name?: ");
 var firstChar = name.slice(0, 1);
 var upperCaseFirtChar = firstChar.toUpperCase();
 var restOfName = name.slice(1, name.length);
